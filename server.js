efd77333dff996dc166113f699f0ccc1649012ac
German <german@MacBook-Air-German.local>
2026-03-29 15:46:09 +0300

    fix: send index.html

--- server.js
+++ server.js
@@ -1,51 +1,33 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: { origin: "*" }
+});
+const path = require('path');
 
-let users = {}; 
+const PORT = process.env.PORT || 3000;
 
-io.on('connection', (socket) => {
-    socket.on('login', (username) => {
-        users[socket.id] = username;
-        io.emit('updateUserList', users);
-        console.log(`${username} вошел в сеть`);
-    });
+// Разрешаем серверу показывать все файлы в папке (картинки, стили, скрипты)
+app.use(express.static(__dirname));
 
-    socket.on('message', (data) => {
-        io.emit('message', data);
-    });
-
-    socket.on('call-user', (data) => {
-        socket.to(data.to).emit('call-made', {
-            offer: data.offer,
-            socket: socket.id,
-            from: users[socket.id]
-        });
-    });
-
-    socket.on('make-answer', (data) => {
-        socket.to(data.to).emit('answer-made', {
-            socket: socket.id,
-            answer: data.answer
-        });
-    });
-
-    socket.on('reject-call', (data) => {
-        socket.to(data.to).emit('call-rejected');
-    });
-
-    socket.on('hangup', (data) => {
-        socket.to(data.to).emit('hangup-received');
-    });
-
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-        io.emit('updateUserList', users);
-    });
+// Главная страница
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(3000, () => console.log('🚀 PELMEN-SERVER LIVE ON PORT 3000'));
+io.on('connection', (socket) => {
+  console.log('Пользователь зашел в чат! 🥟');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Рассылаем всем
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Кто-то ушел кушать пельмени... 💨');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`🚀 СЕРВЕР ПЕЛЬМЕНЕЙ ЗАПУЩЕН НА ПОРТУ ${PORT}`);
+});
